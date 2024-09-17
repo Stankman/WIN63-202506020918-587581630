368dd23dd572c126a832a5cbc0fc1875d8c0fb3b
Diddyy <Diddyy@users.noreply.github.com>
2024-09-17 18:28:57 +0100

    WIP: Add NavigatorCollapsedCategories Serializer

--- PluginRevision.cs
+++ PluginRevision.cs
@@ -123,5 +123,6 @@ public class PluginRevision : IRevision
         { typeof(NavigatorMetaDataMessage), new NavigatorMetaDataSerializer() },
         { typeof(NavigatorSavedSearchesMessage), new NavigatorSavedSearchesSerializer() },
         { typeof(NavigatorEventCategoriesMessage), new NavigatorEventCategoriesSerializer() },
+        { typeof(NavigatorCollapsedCategoriesMessage), new NavigatorCollapsedCategoriesMessageSerializer() },
     };
 }
